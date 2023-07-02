7f37ad0b3054c3a01e5e27d82912f4a6aaddc764
bobo <q463250938@gmail.com>
2023-07-02 15:25:50 -0700

    bug fixes to mustache2 not being able to toggle on/off

--- ObjectAndFaceAnchors/ContentView.swift
+++ ObjectAndFaceAnchors/ContentView.swift
@@ -9,6 +9,25 @@ import SwiftUI
 import RealityKit
 import ARKit
 
+
+//reuseable view Modifiers for mustache views
+struct MustacheModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 110, height: 110)
+            .background(Color.clear)
+            .clipShape(Capsule())
+            .padding()
+    }
+}
+
+extension View {
+    func applyMustacheModifier() -> some View {
+        self.modifier(MustacheModifier())
+    }
+}
+
+
 struct ContentView : View {
     @StateObject var vm = ViewModel()
     
@@ -23,13 +42,24 @@ struct ContentView : View {
                             .resizable()
                             .scaledToFit()
                     }
-                    .frame(width: 110, height: 110)
                     .onTapGesture {
+                        print("Mustache1 Tapped!")
                         vm.toggleMustache1()
                     }
-                    .background(Color.clear)
-                    .clipShape(Capsule())
-                    .padding()
+                    
+                    //custom viewModifier for recycable codes
+                    .applyMustacheModifier()
+                    
+                    VStack{
+                        Image("mustache2")
+                            .resizable()
+                            .scaledToFit()
+                    }
+                    .onTapGesture{
+                        print("Mustache2 tapped!")
+                        vm.toggleMustache2()
+                    }
+                    .applyMustacheModifier()
                 }
                 
             }
--- ObjectAndFaceAnchors/ViewModel.swift
+++ ObjectAndFaceAnchors/ViewModel.swift
@@ -14,33 +14,45 @@ class ViewModel: ObservableObject{
     @Published var arView: ARView = ARView(frame: .zero)
     
     func configureARView() {
-           if !ARFaceTrackingConfiguration.isSupported {
-               print("Your device does not support Face Anchor!")
-           } else {
-               
-               let configuration = ARFaceTrackingConfiguration()
-               arView.session.run(configuration)
-               
-               let faceAnchor = try! Experience.loadScene()
-               
-               let mustache1 = faceAnchor.findEntity(named: "mustache1")
-               mustache1?.isEnabled = false
-               
-               let mustache2 = faceAnchor.findEntity(named: "mustache2")
-               mustache2?.isEnabled = false
-
-               // Add the box anchor to the scene
-               arView.scene.anchors.append(faceAnchor)
-           }
+        
+        let configuration = ARFaceTrackingConfiguration()
+        arView.session.run(configuration)
+        
+        let faceAnchor = try! Experience.loadScene()
+        
+        let mustache1 = faceAnchor.findEntity(named: "mustache1")
+        mustache1?.isEnabled = false
+        
+        let mustache2 = faceAnchor.findEntity(named: "mustache2")
+        mustache2?.isEnabled = false
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(faceAnchor)
+        
     }
     
     
     func toggleMustache1(){
         let mustache1 = self.arView.scene.findEntity(named: "mustache1")
+        let mustache2 = self.arView.scene.findEntity(named: "mustache2")
+        
         if mustache1?.isEnabled == true {
             mustache1?.isEnabled = false
         } else {
             mustache1?.isEnabled = true
+            mustache2?.isEnabled = false
+        }
+    }
+    
+    func toggleMustache2(){
+        let mustache1 = self.arView.scene.findEntity(named: "mustache1")
+        let mustache2 = self.arView.scene.findEntity(named: "mustache2")
+        
+        if mustache2?.isEnabled == true {
+            mustache2?.isEnabled = false
+        } else {
+            mustache2?.isEnabled = true
+            mustache1?.isEnabled = false
         }
     }
 }
